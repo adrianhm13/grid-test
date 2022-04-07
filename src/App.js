abcd35f6a2efa5d945aca62ff70ce8000490ad9e
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-04-07 21:58:32 +0200

    added grid test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid" style={{ border: "1px red" }}>
+      <div className="row">
+        <div className="col-2">A</div>
+        <div className="col-10">
+          <div className="container-fluid" style={{ height: "500px" }}>
+            <div className="row gap-2 h-100 p-0">
+              <div className="col-lg-3 p-0" style={{backgroundColor: 'red'}}>Picture 1</div>
+              <div className="col-lg-5 p-0" style={{backgroundColor: 'cyan'}}>Picture 2</div>
+              <div className="col-lg-3 p-0 overflow-hidden">
+                <p className="mb-1 h-50" style={{backgroundColor: 'green'}}>test</p>
+                <p className="mt-1 h-50" style={{backgroundColor: 'yellow'}}>Test</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
